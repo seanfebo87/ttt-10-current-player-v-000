cb98023cf80a986970121ca4d7f985af37219dd0
Sean Febo <seanfebo@techie.com>
2018-04-03 21:59:11 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,8 +2,8 @@ def turn_count(board)
   counter = 0 
   board.each do |space|
     if space == "X" or "O"
-      counter += 
-      return counter.to_i
-end
+      counter = counter + 1 
+    end
 end 
+return counter 
 end
